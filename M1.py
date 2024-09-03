34bac8c3fef1829dbeba9746e202d6238b55ab05
Kristian H <kristianh@Kristian-sin-MacBook-Air.local>
2024-09-03 09:31:38 +0200

    first commit

---
+++ M1.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+import os
+
+#Variables
+PROJ = 'M1'
+doc = 'impurities_24-1.csv'
+categories = ['Day', 'Filtration', 'Surfactant', 'Sonication'] #Independent
+response = 'response' #Dependent
+
+#Dirs =? 
+cwd = os.getcwd()
+print(cwd)
+cwd = cwd.replace("\\", "/")
+projdir = cwd + '/Projects/' + PROJ + '/'
+resultsdir = projdir + 'ANOVA/'
+
+#Work folders 
+folds = [projdir,resultsdir]
+for fold in folds:
+    if not os.path.exists(fold):
+        os.makedirs(fold)
+
+#Data
+fil = projdir + doc 
+df = pd.read_csv(fil, sep="\t")
+print(df)
